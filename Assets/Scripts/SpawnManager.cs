3c709ac46b53ffa03ad7aaabaee4128d969394ca
Joelcfn <elkiraque@gmail.com>
2025-07-13 15:57:04 +0100

    Update SpawnManager.cs
    2. Spawn the animals at timed intervals

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,31 +1,38 @@
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
-{   public GameObject[] animalPrefabs;
+{
+    public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
-private float spawnRangeZ = 20;
+    private float SpawnPosZ = 20;
+private float startDelay = 2;
+private float spawnInterval = 1.5f;
+
+
 
     void Start()
     {
-        
+            InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKeyDown(KeyCode.S)) {
-        // Randomly generate animal index and spawn position
-        Vector3 spawnPos = new Vector3(
-            Random.Range(-spawnRangeX, spawnRangeX),
-            0,
-            Random.Range(-spawnRangeZ, spawnRangeZ)
-        );
+       
+    }
+
+    void SpawnRandomAnimal() {
+  
         int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, SpawnPosZ);
+
+        // 3) Instancia o prefab na posição e rotação originais do prefab
         Instantiate(
             animalPrefabs[animalIndex],
             spawnPos,
-            animalPrefabs[animalIndex].transform.rotation
-        );
-    }
-    }
+            animalPrefabs[animalIndex].transform.rotation);
 }
+
+}
+
